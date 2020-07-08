75b28294889cc753b33d990d9286c96de1160f83
Shagun Katoch <47280577+shag527@users.noreply.github.com>
2020-07-08 17:07:59 +0530

    Update logistic_regression.py

--- Code/Classification/logistic_regression.py
+++ Code/Classification/logistic_regression.py
@@ -20,10 +20,10 @@ from sklearn.model_selection import cross_val_score
 import seaborn as sbn
 from sklearn.linear_model import LogisticRegression
 
-data=pd.read_csv('/content/drive/My Drive/Colab Notebooks/ISL Recognition/Saved Files/train.csv',low_memory=False)
+data=pd.read_csv('cnn files/train.csv',low_memory=False)
 data.head()
 
-test=pd.read_csv('/content/drive/My Drive/Colab Notebooks/ISL Recognition/Saved Files/test.csv',low_memory=False)
+test=pd.read_csv('cnn files/test.csv',low_memory=False)
 test.head()
 
 x=data.iloc[:,0:180]
