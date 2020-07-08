4505d68045f811f90c331716328720fe8a1041a4
shag527 <shagunkatoch527@gmail.com>
2020-07-08 16:05:07 +0530

    Main files added

--- Website/Recognise_signs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecogniseSignsConfig(AppConfig):
-    name = 'Recognise_signs'
--- Website/Recognise_signs/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-
-# Create your views here.
-def home(request):
-	return render(request,'index.html')
